a254bea441a1e1fadf99744cfb3ca7111259b4ab
Adam Roman Kosiorek <adamk@robots.ox.ac.uk>
2018-11-21 18:43:51 +0000

    add curriculum; it doesn't train without it

--- scripts/train_multi_mnist.sh
+++ scripts/train_multi_mnist.sh
@@ -5,6 +5,8 @@ python scripts/experiment.py\
     --results_dir results\
     --run_name multi_mnist\
     --data_config configs/seq_mnist_data.py\
-    --model_config configs/mlp_mnist_model.py
+    --model_config configs/mlp_mnist_model.py\
+    --seq_len 3\
+    --stage_itr 100000
 
 cd -
